412d7cdae987255a12b2efd7766ebcfc4430d63f
William Correia <william.correia@edu.ece.fr>
2025-06-05 00:39:48 +0200

    connexion bdd + schema User + UserRepo create + prototype signup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const UserRepository = require("./repositories/UserRepository");
 const app = express();
 const port = 3000;
 
+mongoose.connect("mongodb://mongodb:27017/piscord")
+app.use(express.json());
+
 app.listen(port, () => {
     console.log(`Backend listening on port ${port}`);
-})
+});
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
-})
+});
+
+app.post("/signup", async (req, res) => {
+    const { email, password, username } = req.body;
+
+    UserRepository.create({ email, password, username });
+
+    res.json("Utilisateur créé !");
+});
